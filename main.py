751687efc00322ffe7fb3c93ce177be22d23cd33
Rapirys <ilyarotan@gmail.com>
2023-11-25 01:20:21 +0200

    task 6

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import random
+
 
 def prime_factorization(n):
     factors = []
@@ -13,6 +15,7 @@ def prime_factorization(n):
         factors.append(n)
     return factors
 
+
 def mobius(n):
     factors = prime_factorization(n)
     unique_factors = set(factors)
@@ -21,6 +24,7 @@ def mobius(n):
         return 0
 
     return (-1) ** len(factors)
+
 
 def euler(n):
     result = n
@@ -38,7 +42,8 @@ def euler(n):
 
     return result
 
-def gcd(a,b):
+
+def gcd(a, b):
     while a != 0 and b != 0:
         if a > b:
             a = a % b
@@ -47,10 +52,12 @@ def gcd(a,b):
 
     return (a + b)
 
+
 def lcm(a, b):
     return abs(a * b) // gcd(a, b)
 
 
+# task2
 def extended_gcd(a, b):
     x, xx, y, yy = 1, 0, 0, 1
     while b:
@@ -60,12 +67,14 @@ def extended_gcd(a, b):
         y, yy = yy, y - yy * q
     return (a, x, y)
 
+
 def mod_inverse(a, m):
     g, x, _ = extended_gcd(a, m)
     if g != 1:
         raise Exception('Inverse does not exist')
     else:
         return x % m
+
 
 def chinese_remainder_theorem(congruences):
     # congruences is a list of tuples (a_i, m_i)
@@ -81,7 +90,8 @@ def chinese_remainder_theorem(congruences):
 
     return x % M
 
-#3
+
+# task 3
 def legendre_symbol(a, p):
     if a % p == 0:
         return 0
@@ -89,6 +99,7 @@ def legendre_symbol(a, p):
         return 1
     else:
         return -1
+
 
 def jacobi_symbol(a, p):
     if p % 2 == 0 or p <= 0:
@@ -102,19 +113,67 @@ def jacobi_symbol(a, p):
     return result
 
 
+# task 6
+def convert_to_base(num, base):
+    result = []
+    while num > 0:
+        result.append(num % base)
+        num //= base
+    return result[::-1]
+
+
+
+
+def cipolla_mult(pair1, pair2, w, p):
+    a, b = pair1
+    c, d = pair2
+    return ((a * c + b * d * w) % p, (a * d + b * c) % p)
+
+
+def cipolla(n, p):
+    phi = p - 1
+    aa = 0
+    for i in range(1, p):
+        if legendre_symbol(i*i-n, p) == -1:
+            aa = i
+            break
+
+    x1 = (aa, 1)
+    x2 = cipolla_mult(x1, x1, aa * aa - n, p)
+    exponent = convert_to_base((p + 1) // 2, 2)
+
+    for i in range(1, len(exponent)):
+        if exponent[i] == 0:
+            x2 = cipolla_mult(x2, x1, aa * aa - n, p)
+            x1 = cipolla_mult(x1, x1, aa * aa - n, p)
+        else:
+            x1 = cipolla_mult(x1, x2, aa * aa - n, p)
+            x2 = cipolla_mult(x2, x2, aa * aa - n, p)
+
+    return x1[0], (-x1[0] % p)
+
+
 if __name__ == '__main__':
-    #1
+    # 1
     print(mobius(4))
     print(euler(42))
-    a=115
-    b=1
-    print(gcd(115,1), lcm(a,b))
-    #2
+    a = 115
+    b = 1
+    print(gcd(115, 1), lcm(a, b))
+    # 2
     congruences = [(1, 2), (2, 3), (6, 7)]
     print(chinese_remainder_theorem(congruences))
-    #3
+    # 3
     a = 3
     b = 9
-    print(legendre_symbol(a,b))
-    print(jacobi_symbol(a,b))
-
+    print(legendre_symbol(a, b))
+    print(jacobi_symbol(a, b))
+    # 6
+    r = cipolla(2, 7)
+    print("Roots of 2 mod 7:", r[0], r[1], r[0] ** 2 % 7, r[1] ** 2 % 7 == 2, 2 % 7 == 2)
+    r = cipolla(8218, 10007)
+    print("Roots of 8218 mod 10007:", r[0], r[1], r[0] ** 2 % 10007 == 8218, r[1] ** 2 % 10007 == 8218)
+    r = cipolla(56, 101)
+    print("Roots of 56 mod 101:", r[0], r[1], r[0] ** 2 % 101 == 56, r[1] ** 2 % 101 == 56)
+    r = cipolla(1, 11)
+    print("Roots of 1 mod 11:", r[0], r[1], r[0] ** 2 % 11 == 1, r[1] ** 2 % 11 == 1)
